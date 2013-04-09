f97020c279fb68b7426d5a0854d2f272c2a6d536
aweiteka <aweiteka@redhat.com>
2013-04-09 10:18:48 -0400

    Modeled more elements:  * toggle C&U utilization (region level) Zone leve:  * join external vmdb  * create zone  * join zone

--- pages/configuration.py
+++ pages/configuration.py
@@ -46,7 +46,27 @@ class Configuration(Base):
         _edit_group_tags = (By.CSS_SELECTOR, "a[title='Edit Red Hat Tags for this Group']")
         _tag_category_selector = (By.ID, "tag_cat")
         _tag_value_selector = (By.ID, "tag_add")
-        _save_tag_edits_button = (By.CSS_SELECTOR, "img[title='Save Changes']")
+        _save_edits_button = (By.CSS_SELECTOR, "img[title='Save Changes']")
+
+        # Region C&U collection 
+        _cu_collect_tab = (By.CSS_SELECTOR, "a[href='#settings_cu_collection']")
+        _collect_all_clusters_checkbox = (By.ID, "all_clusters")
+        _collect_all_datastores_checkbox = (By.ID, "all_storages")
+
+        # database
+        _database_tab = (By.CSS_SELECTOR, "a[href='#settings_database']")
+        _database_selector= (By.ID, "production_dbtype")
+        _database_host_field= (By.ID, "production_host")
+        _validate_database_button = (By.CSS_SELECTOR, "img[title='Validate Database Configuration']")
+        _save_database_button = (By.CSS_SELECTOR, "img[title='Save changes and restart the Server']")
+
+        # zone
+        _view_zones_link = (By.CSS_SELECTOR, "div[title='View Zones']")
+        _create_new_zone_button = (By.CSS_SELECTOR, "a[title='Add a new zone']")
+        _zone_name_field = (By.ID, "name")
+        _zone_description_field = (By.ID, "description")
+        _save_zone_button = (By.CSS_SELECTOR, "img[title='Add']")
+        _server_zone_selector = (By.ID, "server_zone")
 
         def __init__(self,setup):
             Base.__init__(self, setup)
@@ -62,8 +82,20 @@ class Configuration(Base):
             return self.selenium.find_element(*self._view_groups_link)
 
         @property
+        def view_zones(self):
+            return self.selenium.find_element(*self._view_zones_link)
+
+        @property
         def add_group(self):
             return self.selenium.find_element(*self._add_new_group_button)
+
+        @property
+        def create_new_zone(self):
+            return self.selenium.find_element(*self._create_new_zone_button)
+
+        @property
+        def save_zone(self):
+            return self.selenium.find_element(*self._save_zone_button)
 
         @property
         def retrieve(self):
@@ -74,8 +106,16 @@ class Configuration(Base):
             return self.selenium.find_element(*self._add_button)
 
         @property
-        def save_tag(self):
-            return self.selenium.find_element(*self._save_tag_edits_button)
+        def save(self):
+            return self.selenium.find_element(*self._save_edits_button)
+
+        @property
+        def save_database(self):
+            return self.selenium.find_element(*self._save_database_button)
+
+        @property
+        def validate(self):
+            return self.selenium.find_element(*self._validate_database_button)
 
         @property
         def edit_tag(self):
@@ -84,6 +124,22 @@ class Configuration(Base):
         @property
         def ldap_lookup_checkbox(self):
             return self.selenium.find_element(*self._ldap_lookup_checkbox)
+
+        @property
+        def cu_collect_tab(self):
+            return self.selenium.find_element(*self._cu_collect_tab)
+
+        @property
+        def database_tab(self):
+            return self.selenium.find_element(*self._database_tab)
+
+        @property
+        def collect_all_clusters_checkbox(self):
+            return self.selenium.find_element(*self._collect_all_clusters_checkbox)
+
+        @property
+        def collect_all_datastores_checkbox(self):
+            return self.selenium.find_element(*self._collect_all_datastores_checkbox)
 
         def click_on_view_groups(self):
             self.view_groups.click()
@@ -116,6 +172,42 @@ class Configuration(Base):
             self._wait_for_results_refresh()
             return Configuration.Configuration(self.testsetup)
 
+        def click_on_cu_collect_tab(self):
+            self.cu_collect_tab.click()
+            self._wait_for_results_refresh()
+            return Configuration.Configuration(self.testsetup)
+
+        def click_on_database_tab(self):
+            self.database_tab.click()
+            self._wait_for_results_refresh()
+            return Configuration.Configuration(self.testsetup)
+
+        def complete_database_form(self, external_cfme_vmdb):
+            self.select_dropdown("External Database on another EVM Appliance", \
+                *self._database_selector)
+            self.selenium.find_element(*self._database_host_field).clear()
+            self.selenium.find_element(*self._database_host_field).send_keys(external_cfme_vmdb)
+            return Configuration.Configuration(self.testsetup)
+
+        def complete_zone_form(self, name, desc):
+            self.selenium.find_element(*self._zone_name_field).send_keys(name)
+            self.selenium.find_element(*self._zone_description_field).send_keys(desc)
+            return Configuration.Configuration(self.testsetup)
+
+        def click_on_save_zone(self):
+            self.save_zone.click()
+            self._wait_for_results_refresh()
+            return Configuration.Configuration(self.testsetup)
+
+        def select_zone(self, zone):
+            self.select_dropdown(zone, *self._server_zone_selector)
+            return Configuration.Configuration(self.testsetup)
+            
+        def click_on_validate_database(self):
+            self.validate.click()
+            self._wait_for_results_refresh()
+            return Configuration.Configuration(self.testsetup)
+
         def select_ldap_lookup(self):
             if not self.ldap_lookup_checkbox.is_selected():
                 return self.ldap_lookup_checkbox.click()
@@ -130,9 +222,26 @@ class Configuration(Base):
             self.add.click()
             return Configuration.Configuration(self.testsetup)
 
-        def click_on_save_tag(self):
-            self.save_tag.click()
+        def click_on_save(self):
+            self.save.click()
+            self._wait_for_results_refresh()
             return Configuration.Configuration(self.testsetup)
+
+        def toggle_collect_all_clusters(self, enable):
+            if enable:
+                if not self.collect_all_clusters_checkbox.is_selected():
+                    return self.collect_all_clusters_checkbox.click()
+            else:
+                if self.collect_all_clusters_checkbox.is_selected():
+                    return self.collect_all_clusters_checkbox.click()
+
+        def toggle_collect_all_datastores(self, enable):
+            if enable:
+                if not self.collect_all_datastores_checkbox.is_selected():
+                    return self.collect_all_datastores_checkbox.click()
+            else:
+                if self.collect_all_datastores_checkbox.is_selected():
+                    return self.collect_all_datastores_checkbox.click()
 
         def create_local_group(self, group, role):
             self.click_on_view_groups()
@@ -159,5 +268,42 @@ class Configuration(Base):
             self.click_on_group_detail(group)
             self.click_on_edit_tags()
             self.select_tag(tag)
-            return self.click_on_save_tag()
+            return self.click_on_save()
+
+        def toggle_region_cu_collection(self, enable_clusters, enable_datastores):
+            self.click_on_cu_collect_tab()
+            self.toggle_collect_all_clusters(enable_clusters)
+            self.toggle_collect_all_datastores(enable_datastores)
+            return self.save.is_displayed()
+
+        def save_changes(self):
+            return self.click_on_save()
+
+        def validate_external_cfme_vmdb(self, appliance, cfme_name, external_cfme_vmdb):
+            appliance.click()
+            self._wait_for_results_refresh()
+            self.click_on_database_tab()
+            self._wait_for_results_refresh()
+            self.complete_database_form(external_cfme_vmdb)
+            return self.click_on_validate_database()
+
+        def save_database_and_restart(self):
+            # assumes validate_external_cfme_vmdb() called
+            self.save_database.click()
+            self.handle_popup()
+            self._wait_for_results_refresh()
+            return Configuration.Configuration(self.testsetup)
+
+        def create_zone(self, name, desc):
+            self.view_zones.click()
+            self._wait_for_results_refresh()
+            self.create_new_zone.click()
+            self._wait_for_results_refresh()
+            self.complete_zone_form(name, desc)
+            return self.click_on_save_zone()
+
+        def select_zone(self, zone):
+            self.select_dropdown(zone, *self._server_zone_selector)
+            self._wait_for_results_refresh()
+            return Configuration.Configuration(self.testsetup)
 
